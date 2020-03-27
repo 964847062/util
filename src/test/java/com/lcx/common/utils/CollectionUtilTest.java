ef42b506dc12b090832226b800c89ce147d05756
liucanxiang <964847062@qq.com>
2020-03-27 15:08:07 +0800

    提交工具类

---
+++ src/test/java/com/lcx/common/utils/CollectionUtilTest.java
@@ -0,0 +1,20 @@
+package com.lcx.common.utils;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class CollectionUtilTest {
+
+	@Test
+	public void testIsEmpty() {
+		ArrayList<Object> list = new ArrayList<Object>();
+		//list.add("aaa");
+		//HashMap<String, String> map = new HashMap<String, String>();
+		boolean b = CollectionUtil.isEmpty(list);
+		System.out.println(b);
+	}
+
+}
